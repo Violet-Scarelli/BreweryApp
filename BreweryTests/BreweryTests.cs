7bd59f44952087858544412a7a64e4bd8d142743
Violet-Scarelli <alphaone150@gmail.com>
2023-12-03 20:49:19 -0800

    updated the code a lil

--- BreweryTests/BreweryTests.cs
+++ BreweryTests/BreweryTests.cs
@@ -3,6 +3,7 @@ using BreweryApp.Classes;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace BreweryTests
 {
@@ -28,9 +29,15 @@ namespace BreweryTests
 		public void GetRecipesTest()
 		{
 			List<Recipe> recipes = dbContext.Recipes.OrderBy(i => i.RecipeId).ToList();
+			List<Ingredient> ingredients = dbContext.Ingredients.OrderBy(i => i.IngredientId).ToList();
 			Assert.AreEqual(4, recipes.Count);
 			Assert.AreEqual(recipes[0].Name, "Fuzzy Tales Juicy IPA");
-			
+			Console.WriteLine(recipes[0].Name);
+			List<RecipeIngredient> selectedIngredients = dbContext.RecipeIngredients.Where(ingredient => ingredient.RecipeId == recipes[0].RecipeId).ToList();
+			foreach(var ingredient in selectedIngredients)
+			{
+				Console.WriteLine(ingredients[ingredient.IngredientId].Name);
+			}
 		}
 
 	}
